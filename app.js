ff1f3559675e64ce8400b3fd30bc5cb1989d0d10
marvelous3500 <marvelous.sylvester@flexisaf.net>
2019-02-08 19:34:39 +0100

    refector server file by add version

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 import express from 'express'
 import bodyParser from 'body-parser'
-import router from './api/routes/usersRouter'
+import userrouter from './api/routes/usersRouter'
 import studentrouter from './api/routes/studentRouter'
 import * as auth from './api/Middleware/auth'
 
@@ -10,9 +10,9 @@ const port = 3000
 app.use(bodyParser.json({ type: 'application/json' }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
-app.use('/api/login', auth.getToken)
-app.use('/api/user', router)
-app.use('/api/student/', studentrouter)
+app.use('/api/v1/login', auth.getToken)
+app.use('/api/v1/user', userrouter)
+app.use('/api/v1/student/', studentrouter)
 
 app.get('/', (req, res) => res.send('welocme to Admission Processing App'))
 app.listen(port, () => console.log(` server is runing  on port ${port}!`))
